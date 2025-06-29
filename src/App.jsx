a93dede4823b4dcc1c6be6800d8bad092d3d294f
aashish-nomad <aashish.nomad@gmail.com>
2025-06-29 15:01:37 +0545

    Adds winning combination file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 import Log from "./components/Log";
+import winningCombinations from "../winning-combinations";
 
 function deriveActivePlayer(gameTurn) {
   let currentPlayer = 'X';
