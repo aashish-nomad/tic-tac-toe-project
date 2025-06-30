3b06c285da9fb86e5dbaf05f936d38f8c00ef1c7
aashish-nomad <aashish.nomad@gmail.com>
2025-06-30 10:15:41 +0545

    Adds game winning logic

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,36 @@ function deriveActivePlayer(gameTurn) {
   return currentPlayer
 }
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+
 function App() {
   const [gameTurn, setGameTurn] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurn);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurn) {
+    const { square, player } = turn;
+    gameBoard[square.row][square.col] = player
+  }
+
+  let winner;
+
+  for (const combination of winningCombinations) {
+    const firstSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSymbol && firstSymbol == secondSymbol && firstSymbol == thirdSymbol) {
+      winner = firstSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
 
@@ -27,15 +53,16 @@ function App() {
     });
   }
 
-
   return (
     <main>
       <div id="game-container">
+
         <ol id="players" className="highlight-player">
           <Player name="Player1" symbol="X" isActive={activePlayer == 'X'} />
           <Player name="Player2" symbol="O" isActive={activePlayer == 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurn} />
+        {<p>Congarats {winner} player you are the winner!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurn} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    gameBoard[square.row][square.col] = player
-  }
+export default function GameBoard({ onSelectSquare, board }) {
 
   return (<ol id="game-board">
-    {gameBoard.map((row, rowIndex) => {
+    {board.map((row, rowIndex) => {
       return <li key={rowIndex}>
         <ol>
           {row.map((col, colIndex) => <li key={colIndex}>
